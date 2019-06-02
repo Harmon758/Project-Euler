741c1079c654d93e287e34b3072e404f96358f35
Harmon <Harmon758@gmail.com>
2019-06-02 13:01:02 -0500

    ProjectEuler+ Challenge #1: Improve C solution
    Remove unnecessary includes
    Improve variable names
    Fix format length specifiers
    Optimize/Improve efficiency
    Improve clarity/coherence
    Improve formatting
    Remove unnecessary comment

--- 001 - Multiples of 3 and 5/ProjectEuler+ Challenge #1 Solution.c
+++ 001 - Multiples of 3 and 5/ProjectEuler+ Challenge #1 Solution.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 
 int main() {
-    long int T = 0, N = 0, Temp = 0;
-    long long int Sum = 0;
-    scanf("%d", &T);
-    for(int i = 0; i < T; i++){
-        Sum = 0;
-        scanf("%d", &N);
-        Temp = (N - 1) / 3;
-        Sum += (Temp * (3 + Temp * 3)) / 2;
-        Temp = (N - 1) / 5;
-        Sum += (Temp * (5 + Temp * 5)) / 2;
-        Temp = (N - 1) / 15;
-        Sum -= (Temp * (15 + Temp * 15)) / 2;
-        printf("%lld\n", Sum);
-    }
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    long int T = 0, N = 0;
+    long int multiples_of_3 = 0, multiples_of_5 = 0, multiples_of_15 = 0;
+    long long int sum = 0;
+    scanf("%ld", &T);
+    for (int _ = 0; _ < T; _++) {
+        scanf("%ld", &N);
+        multiples_of_3 = (N - 1) / 3;
+        multiples_of_5 = (N - 1) / 5;
+        multiples_of_15 = (N - 1) / 15;
+        sum = 3 * multiples_of_3 * (multiples_of_3 + 1) / 2
+              + 5 * multiples_of_5 * (multiples_of_5 + 1) / 2
+              - 15 * multiples_of_15 * (multiples_of_15 + 1) / 2;
+        printf("%lld\n", sum);
+    }  
     return 0;
 }
