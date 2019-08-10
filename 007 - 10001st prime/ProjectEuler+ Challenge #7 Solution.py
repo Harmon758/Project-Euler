e0212e42a0fdda90db8df96d8180358d43c8e6e7
Harmon <Harmon758@gmail.com>
2019-08-10 13:56:13 -0500

    ProjectEuler+ Challenge #7: Improve Python solution
    Use Python 3
    Improve variable names
    Optimize/Improve efficiency
    Calculate limit instead of hardcoding value
    Improve clarity/coherence
    Improve formatting
    Remove unnecessary comment

--- 007 - 10001st prime/ProjectEuler+ Challenge #7 Solution.py
+++ 007 - 10001st prime/ProjectEuler+ Challenge #7 Solution.py
@@ -1,14 +1,44 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-A = [True]*104744
-T = int(raw_input())
-for i in range(T):
-	Count = 0
-	N = int(raw_input())
-	for j in range(2, 104744):
-		if A[j] == True:
-			Count += 1
-			if Count == N:
-				print j
-				break
-			for k in range(j * j, 104744, j):
-				A[k] = False
+from math import ceil, log
+
+N_upper_bound = 10 ** 4 + 1  # 10001 (+ 1) for Project Euler Problem
+limit = ceil(N_upper_bound * (log(N_upper_bound) + log(log(N_upper_bound))))
+
+# n-th prime number < n * (log n + log log n) for n >= 6
+
+# Rosser, Barkley.
+# "Explicit Bounds for Some Functions of Prime Numbers."
+# American Journal of Mathematics, vol. 63, no. 1, 1941, pp. 211-232.
+# DOI: 10.2307/2371291.
+# JSTOR, www.jstor.org/stable/2371291.
+# Theorem 11 / Theorem 28
+
+# Dusart, Pierre.
+# "The kth prime is greater than k(log k + log log k-1) for k >= 2."
+# Mathematics of Computation, vol. 68, no. 225, 1999, pp. 411-415.
+# DOI: 10.1090/S0025-5718-99-01037-6.
+# AMS, www.ams.org/journals/mcom/1999-68-225/S0025-5718-99-01037-6/home.html.
+# JSTOR, www.jstor.org/stable/2585122.
+# Lemma 1
+
+# Unicode replaced with ASCII in citations for HackerRank:
+# "[W]e don't accept submissions with non ASCII charaters [sic] for this
+# challenge."
+
+limit += 1
+numbers = [True] * limit
+primes = []
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    if len(primes) >= N:
+        print(primes[N - 1])
+        continue
+    for number in range(next(reversed(primes), 1) + 1, limit):
+        if numbers[number]:
+            primes.append(number)
+            for multiple in range(number * number, limit, number):
+                numbers[multiple] = False
+            if len(primes) == N:
+                print(number)
+                break
