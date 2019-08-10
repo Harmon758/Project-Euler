1bdba8607a03ad73cf4d06eb34a156519c87dfd0
Harmon <Harmon758@gmail.com>
2019-08-10 16:13:11 -0500

    ProjectEuler+ Challenge #8: Improve Python solution
    Use Python 3
    Optimize/Improve efficiency
    Improve clarity/coherence
    Improve formatting
    Improve variable names
    Remove unnecessary comment

--- 008 - Largest product in a series/ProjectEuler+ Challenge #8 Solution.py
+++ 008 - Largest product in a series/ProjectEuler+ Challenge #8 Solution.py
@@ -1,13 +1,19 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-T = int(raw_input())
-for i in range (0, T):
-    N, K = map(int, raw_input().split())
-    Number = int(raw_input())
-    Max_Product = 0
-    for j in range(0, N - K):
-        Product = 1
-        for k in range(j, j + K):
-            Product *= int(str(Number)[k - 1])
-        if Product > Max_Product:
-            Max_Product = Product
-    print Max_Product
+from functools import reduce
+from operator import mul
+
+T = int(input())
+for _ in range(T):
+    N, K = map(int, input().split())
+    digits = list(map(int, input()))
+    max_product = product = reduce(mul, digits[:K])
+    # Use math.prod in Python 3.8
+    for index in range(1, N - K):
+        try:
+            product //= digits[index - 1]
+            product *= digits[index + K - 1]
+        except ZeroDivisionError:
+            product = reduce(mul, digits[index:index + K])
+            # Use math.prod in Python 3.8
+        if product > max_product:
+            max_product = product
+    print(max_product)
