018439c5bb7d578c1f2d576d57732acd8027f24b
Harmon <Harmon758@gmail.com>
2019-08-10 15:20:10 -0500

    ProjectEuler+ Challenge #7: Improve C solution
    Optimize/Improve efficiency
    Calculate limit instead of hardcoding value
    Improve include order
    Improve variable names
    Improve clarity/coherence
    Improve formatting
    Remove unnecessary include
    Remove unnecessary comment

--- 007 - 10001st prime/ProjectEuler+ Challenge #7 Solution.c
+++ 007 - 10001st prime/ProjectEuler+ Challenge #7 Solution.c
@@ -1,36 +1,70 @@
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
 int main() {
-    int T, N, Count;
-    bool A[104744];
+    int N_upper_bound = 10001;  // 10001 (+ 1) for Project Euler Problem
+    long int limit = ceil(N_upper_bound *
+                          (log(N_upper_bound) + log(log(N_upper_bound))));
+
+    // n-th prime number < n * (log n + log log n) for n >= 6
+
+    /*
+    Rosser, Barkley
+    "Explicit Bounds for Some Functions of Prime Numbers"
+    American Journal of Mathematics, vol. 63, no. 1, 1941, pp. 211-232
+    DOI: 10.2307/2371291
+    JSTOR, www.jstor.org/stable/2371291
+    Theorem 11 / Theorem 28
+    */
+
+    /*
+    Dusart, Pierre
+    "The kth prime is greater than k(log k + log log k-1) for k >= 2"
+    Mathematics of Computation, vol. 68, no. 225, 1999, pp. 411-415
+    DOI: 10.1090/S0025-5718-99-01037-6
+    AMS, www.ams.org/journals/mcom/1999-68-225/S0025-5718-99-01037-6/home.html
+    JSTOR, www.jstor.org/stable/2585122
+    Lemma 1
+    */
+
+    /*
+    Unicode replaced with ASCII in citations for HackerRank:
+    "[W]e don't accept submissions with non ASCII charaters [sic] for this
+    challenge."
+    */
+
+    bool numbers[++limit];
+    memset(numbers, true, (limit) * sizeof(bool));
+    long int primes[N_upper_bound];
+    int primes_count = 0;
+
+    int T, N;
     scanf("%d", &T);
-    for(int i = 0; i < T; i++){
-        Count = 0;
-        for(int j = 0; j <= 104743; j++){
-            A[j] = true;
-        }
+    for (int _ = 0; _ < T; _++) {
         scanf("%d", &N);
-        for(int j = 2; j <= sqrt(104743); j++){
-            if(A[j] == true){
-                for(int k = j * j; k <= 104743; k += j){
-                    A[k] = false;
+        if (primes_count >= N) {
+            printf("%ld\n", primes[N - 1]);
+            continue;
+        }
+        long int number = 2;
+        if (primes_count != 0) {
+            number = primes[primes_count - 1] + 1;
+        }
+        for (; number < limit; number++) {
+            if (numbers[number]) {
+                primes[primes_count++] = number;
+                for (long int multiple = number * number; multiple < limit;
+                     multiple += number) {
+                    numbers[multiple] = false;
+                }
+                if (primes_count == N) {
+                    printf("%ld\n", number);
+                    break;
                 }
             }
         }
-        for(int j = 2; j <= 104743; j++){
-            if(A[j] == true){
-                Count++;
-            }
-            if(Count == N){
-                printf("%d\n", j);
-                break;
-            }
-        }
     }
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
 }
