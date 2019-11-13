c89d8bb26a57d093a693972338f18e9c707eda02
Harmon <Harmon758@gmail.com>
2019-11-13 09:00:07 -0600

    ProjectEuler+ Challenge #9: Improve Python solution
    Use Python 3
    Optimize/Improve efficiency
    Improve clarity/coherence
    Improve formatting
    Improve variable names
    Remove unnecessary comment

--- 009 - Special Pythagorean triplet/ProjectEuler+ Challenge #9 Solution.py
+++ 009 - Special Pythagorean triplet/ProjectEuler+ Challenge #9 Solution.py
@@ -1,14 +1,13 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-T = int(raw_input())
-for i in range(T):
-	N = int(raw_input())
-	Largest = 0
-	for j in range(1, N / 3 + 1):
-		k = (2 * j * N - N * N) / (2 * j - 2 * N)
-		l = N - k - j
-		if l * l == k * k +  j * j and l * k * j > Largest:
-			Largest = l * k * j
-	if Largest == 0:
-		print "-1"
-	else:
-		print Largest
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    largest = -1
+    for a in range(1, N // 3 + 1):
+        b = N * (a - N // 2) // (a - N)
+        c = N - a - b
+        if a * a + b * b == c * c:
+            product = a * b * c
+            # Use := in Python 3.8
+            if product > largest:
+                largest = product
+    print(largest)
