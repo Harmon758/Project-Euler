f7ec6bec0e842218b786efe5b915d6c4e400c75f
Harmon <Harmon758@gmail.com>
2019-11-14 07:18:55 -0600

    ProjectEuler+ Challenge #11: Improve Python solution
    Use Python 3
    Optimize/Improve efficiency
    Improve clarity/coherence
    Improve formatting
    Improve variable names
    Remove unnecessary comment

--- 011 - Largest product in a grid/ProjectEuler+ Challenge #11 Solution.py
+++ 011 - Largest product in a grid/ProjectEuler+ Challenge #11 Solution.py
@@ -1,16 +1,15 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-Grid = []
-Largest = 0
-for i in range(0, 20):
-    Grid.append(map(int, raw_input().split()))
-for i in range(0, 20):
-    for j in range(0, 20):
-        if i <= 16 and Grid[i][j] * Grid[i + 1][j] * Grid[i + 2][j] * Grid[i + 3][j] > Largest:
-            Largest = Grid[i][j] * Grid[i + 1][j] * Grid[i + 2][j] * Grid[i + 3][j]
-        if j <= 16 and Grid[i][j] * Grid[i][j + 1] * Grid[i][j + 2] * Grid[i][j + 3] > Largest:
-            Largest = Grid[i][j] * Grid[i][j + 1] * Grid[i][j + 2] * Grid[i][j + 3]
-        if i <= 16 and j <= 16 and Grid[i][j] * Grid[i + 1][j + 1] * Grid[i + 2][j + 2] * Grid[i + 3][j + 3] > Largest:
-            Largest = Grid[i][j] * Grid[i + 1][j + 1] * Grid[i + 2][j + 2] * Grid[i + 3][j + 3]
-        if i >= 3 and j <= 16 and Grid[i][j] * Grid[i - 1][j + 1] * Grid[i - 2][j + 2] * Grid[i - 3][j + 3] > Largest:
-            Largest = Grid[i][j] * Grid[i - 1][j + 1] * Grid[i - 2][j + 2] * Grid[i - 3][j + 3]
-print Largest
+from functools import reduce
+from operator import mul
+
+grid = [[0] * 23] * 3
+for _ in range(20):
+    grid.append(list(map(int, input().split())) + [0] * 3)
+grid.extend([[0] * 23] * 3)
+print(max(
+    reduce(mul, [
+        grid[row + direction[0] * offset][column + direction[1] * offset]
+        for offset in range(4)
+    ])
+    for direction in ((0, 1), (1, 0), (1, 1), (-1, 1))
+    for row in range(3, 23) for column in range(20)
+))
