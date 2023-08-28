16387b64029a2ca20d359731a870b2c3fff5ba3f
Harmon <Harmon758@gmail.com>
2023-08-27 23:53:25 -0500

    [8] Update Python solution to use math.prod
    Now that HackerRank uses Python >= 3.8
    https://candidatesupport.hackerrank.com/hc/en-us/articles/4402913877523-Execution-Environment-and-Samples

--- 008 - Largest product in a series/ProjectEuler+ Challenge #8 Solution.py
+++ 008 - Largest product in a series/ProjectEuler+ Challenge #8 Solution.py
@@ -1,19 +1,16 @@
-from functools import reduce
-from operator import mul
+from math import prod
 
 T = int(input())
 for _ in range(T):
     N, K = map(int, input().split())
     digits = list(map(int, input()))
-    max_product = product = reduce(mul, digits[:K])
-    # Use math.prod in Python 3.8
+    max_product = product = prod(digits[:K])
     for index in range(1, N - K):
         try:
             product //= digits[index - 1]
             product *= digits[index + K - 1]
         except ZeroDivisionError:
-            product = reduce(mul, digits[index:index + K])
-            # Use math.prod in Python 3.8
+            product = prod(digits[index:index + K])
         if product > max_product:
             max_product = product
     print(max_product)
