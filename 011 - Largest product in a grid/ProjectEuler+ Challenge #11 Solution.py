97db092f7592cce603f067130e5a2bdfe37f755b
Harmon <Harmon758@gmail.com>
2023-08-28 09:58:04 -0500

    [11] Update Python solution to use math.prod
    Now that HackerRank uses Python >= 3.8
    https://candidatesupport.hackerrank.com/hc/en-us/articles/4402913877523-Execution-Environment-and-Samples

--- 011 - Largest product in a grid/ProjectEuler+ Challenge #11 Solution.py
+++ 011 - Largest product in a grid/ProjectEuler+ Challenge #11 Solution.py
@@ -1,15 +1,14 @@
-from functools import reduce
-from operator import mul
+from math import prod
 
 grid = [[0] * 23] * 3
 for _ in range(20):
     grid.append(list(map(int, input().split())) + [0] * 3)
 grid.extend([[0] * 23] * 3)
 print(max(
-    reduce(mul, [
+    prod(
         grid[row + direction[0] * offset][column + direction[1] * offset]
         for offset in range(4)
-    ])
+    )
     for direction in ((0, 1), (1, 0), (1, 1), (-1, 1))
     for row in range(3, 23) for column in range(20)
 ))
