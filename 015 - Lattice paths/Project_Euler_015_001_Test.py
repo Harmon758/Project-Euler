37340bc900eff52e43a26826391ae44fc0c29aa3
Harmon <Harmon758@gmail.com>
2023-08-29 12:07:28 -0500

    [15] Remove old solutions

--- 015 - Lattice paths/Project_Euler_015_001_Test.py
+++
@@ -1,9 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-T = int(raw_input())
-for i in range(T):
-    N, M = map(int, raw_input().split())
-    k = min(N, M)
-    paths = 1
-    for j in range(k):
-        paths = paths * (N + M - j) / (j + 1)
-    print paths
