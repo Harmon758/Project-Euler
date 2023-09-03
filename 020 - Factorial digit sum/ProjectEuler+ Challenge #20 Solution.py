014d7d3575bbfd90b46e9275b79067cc983251b6
Harmon <Harmon758@gmail.com>
2023-09-03 12:19:27 -0500

    [20] Improve Python solution
    Use Python 3
    Optimize/Improve efficiency
    Improve clarity/coherence
    Improve formatting
    Improve variable names
    Remove unnecessary comment

--- 020 - Factorial digit sum/ProjectEuler+ Challenge #20 Solution.py
+++ 020 - Factorial digit sum/ProjectEuler+ Challenge #20 Solution.py
@@ -1,12 +1,11 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-import math
+from math import factorial
 
-T = int(raw_input())
-for i in range(T):
-    N = int(raw_input())
-    Number = math.factorial(N)
-    Sum = 0
-    while Number != 0:
-        Sum += Number % 10
-        Number /= 10
-    print Sum
+T = int(input())
+for test_case in range(T):
+    N = int(input())
+    number = factorial(N)
+    sum_of_digits = 0
+    while number:
+        number, remainder = divmod(number, 10)
+        sum_of_digits += remainder
+    print(sum_of_digits)
