179a22afcaa0b02aecbaeda0b76011d14a373d74
Harmon <Harmon758@gmail.com>
2023-09-22 06:19:21 -0500

    [218] Add C solution

---
+++ 218 - Perfect Right-angled Triangles/ProjectEuler+ Challenge #218 Solution.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int q;
+    long long int n;
+    scanf("%d", &q);
+    for (int query = 0; query < q; query++) {
+        scanf("%lld", &n);
+        printf("%d\n", 0);
+    }  
+    return 0;
+}
